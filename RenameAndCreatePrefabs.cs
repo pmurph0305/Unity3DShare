ab77e246b1c4b234ef560b7f556cddb87e681fd0
unknown <pmurph0305@gmail.com>
2020-10-22 08:51:18 -0400

    Backup old found scripts

---
+++ RenameAndCreatePrefabs.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class RenameAndCreatePrefabs : EditorWindow
+{
+
+    [MenuItem("Window/RenameAndCreatePrefabs")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof (RenameAndCreatePrefabs));
+    }
+
+
+    private Material material;
+    private string textToReplace;
+    private string newText;
+    private string prefabLocation;
+
+    private bool isSkinned;
+    void OnGUI()
+    {
+        GUILayout.Label("Tools to rename a bunch of selected gameobjects, apply material, and create prefabs");
+
+        material = (Material) EditorGUILayout.ObjectField("Material", material, typeof (Material));
+        textToReplace = EditorGUILayout.TextField("Text To Replace", textToReplace);
+        newText = EditorGUILayout.TextField("New Text", newText);
+        prefabLocation = EditorGUILayout.TextField("PrefabFolder:", prefabLocation);
+        isSkinned = EditorGUILayout.Toggle("Skinned Object?", isSkinned);
+        if (GUILayout.Button("Create Prefabs"))
+        {
+
+            GameObject[] selection = Selection.gameObjects;
+            foreach (GameObject gameObj in selection)
+            {
+                string name = gameObj.name;
+                name = name.Replace(textToReplace, newText);
+                gameObj.name = name;
+                if (!isSkinned)
+                {
+                    Renderer rend = gameObj.GetComponent<Renderer>();
+                    rend.material = material;
+                }
+                else
+                {
+                    SkinnedMeshRenderer skinnedMeshRenderer = gameObj.GetComponentInChildren<SkinnedMeshRenderer>();
+                    if (skinnedMeshRenderer != null)
+                    {
+                        skinnedMeshRenderer.material = material;
+                    }
+                }
+                UnityEngine.Object newPrefab = PrefabUtility.CreateEmptyPrefab(prefabLocation +  name + ".prefab");
+                PrefabUtility.ReplacePrefab(gameObj, newPrefab,ReplacePrefabOptions.ConnectToPrefab);
+            }
+
+        }
+
+        GUILayout.Label("Location format: Assets/Folder/Prefabs/");
+    }
+
+}
---
+++ Sharpen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(SharpenRenderer), PostProcessEvent.AfterStack, "Custom/Sharpen")]
+public sealed class Sharpen : PostProcessEffectSettings
+{
+    [Range(0f, 5f), Tooltip("Sharpen effect intensity.")]
+    public FloatParameter sharpen = new FloatParameter { value = 0.5f };
+}
+
+public sealed class SharpenRenderer : PostProcessEffectRenderer<Sharpen>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Sharpen"));
+        sheet.properties.SetFloat("_Sharpness", settings.sharpen);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
